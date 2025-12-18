79412c42d472ca385453de6ebb7147e8277d4a81
Yehuda Katz <wycats@gmail.com>
2025-12-18 13:08:53 -0800

    locald-vmm: gate boot params example to x86

--- locald-vmm/examples/find_boot_params.rs
+++ locald-vmm/examples/find_boot_params.rs
@@ -1,6 +1,12 @@
 //! Minimal example used for debugging the boot params type.
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 fn main() {
     // Try to find boot_params
     let _ = linux_loader::bootparam::boot_params::default();
+}
+
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+fn main() {
+    // `linux_loader` only exposes x86/x86_64 boot params.
 }
